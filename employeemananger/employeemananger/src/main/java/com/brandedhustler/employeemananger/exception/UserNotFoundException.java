28217f9379dddbb1fa3dd030e84c5d3e6a1d865d
Eselase-Noble <nevulley@st.ug.edu.gh>
2022-06-22 11:26:00 +0000

    API, MODELS, created

---
+++ employeemananger/employeemananger/src/main/java/com/brandedhustler/employeemananger/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.brandedhustler.employeemananger.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+
+        super(message);
+    }
+}
---
+++ employeemananger/employeemananger/src/main/java/com/brandedhustler/employeemananger/service/EmployeeService.java
@@ -0,0 +1,44 @@
+package com.brandedhustler.employeemananger.service;
+
+import com.brandedhustler.employeemananger.exception.UserNotFoundException;
+import com.brandedhustler.employeemananger.model.Employee;
+import com.brandedhustler.employeemananger.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() ->new UserNotFoundException("User by id " + id + " not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+}
